38c39cd141bfceb1c9a577e8d6c0c3f12b1fd19c
AlexBryksin <alexander.bryksin@yandex.ru>
2020-01-02 20:30:36 +0300

    add pie chart initial render on update

--- js/graph.js
+++ js/graph.js
@@ -6,31 +6,64 @@ const dims = {
   radius: 150,
 }
 const center = { x: dims.width / 2 + 5, y: dims.height / 2 + 5 }
-const svg = d3.select('.canvas')
-
-// create svg
-svg
+const svg = d3
+  .select('.canvas')
   .append('svg')
   .attr('width', dims.width + 150)
   .attr('height', dims.height + 150)
 
-// Create graph group
-const graph = svg.append('g')
-graph.attr('transform', `translate(${center.x}, ${center.y})`)
+const graph = svg.append('g').attr('transform', `translate(${center.x}, ${center.y})`)
+// translates the graph group to the middle of the svg container
 
 // Create pie chart
-const pie = d3.pie()
-pie.sort(null).value((d) => d.cost) // for generate pie chart angles
+const pie = d3
+  .pie()
+  .sort(null)
+  .value((d) => d.cost) // the value we are evaluating to create the pie angles
 
-const angles = pie([
-  { name: 'rent', cost: 500 },
-  { name: 'bills', cost: 300 },
-  { name: 'gaming', cost: 200 },
-])
-
-const arcPath = d3.arc()
-arcPath
+const arcPath = d3
+  .arc()
   .outerRadius(dims.radius)
   .innerRadius(dims.radius / 2)
 
-console.log(arcPath(angles[0]))
+// Update data
+const update = (data) => {
+  // join enhanced (pie) data to path elements
+  const paths = graph.selectAll('path').data(pie(data))
+
+  console.log(paths)
+
+  paths
+    .enter()
+    .append('path')
+    .attr('class', 'arc')
+    .attr('d', arcPath)
+    .attr('stroke', '#fff')
+    .attr('stroke-width', 3)
+}
+
+// Get data from firebase firestore
+let data = []
+db.collection('expenses').orderBy('cost').onSnapshot((res) => {
+  res.docChanges().forEach((change) => {
+    const doc = { ...change.doc.data(), id: change.doc.id }
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc)
+        break
+      case 'modified':
+        const index = data.findIndex((item) => item.id === doc.id)
+        data[index] = doc
+        break
+      case 'removed':
+        data = data.filter((item) => item.id !== doc.id)
+        break
+
+      default:
+        break
+    }
+  })
+  console.log(data)
+  update(data)
+})
